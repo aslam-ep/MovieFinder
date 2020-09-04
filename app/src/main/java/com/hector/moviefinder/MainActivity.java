1cb3ce7ee8c282df010008a540c27a0ea64f6eab
Mohammed Aslam Ep <aslamep.ssmptc@gmail.com>
2020-09-05 01:26:29 +0530

    Update MainActivity.java

--- app/src/main/java/com/hector/moviefinder/MainActivity.java
+++ app/src/main/java/com/hector/moviefinder/MainActivity.java
@@ -54,8 +54,8 @@ another one for getting the Rotten Tomatoes rating for that movie
 public class MainActivity extends AppCompatActivity {
 
     // API KEYS
-    String TASTDIVE_API_KEY = "383937-MovieFin-7XRETLZZ";
-    String OMDB_API_KEY = "f2c0d5ef";
+    String TASTDIVE_API_KEY = "<PAST API KEY>";
+    String OMDB_API_KEY = "<PAST API KEY>";
 
     // Variable for double tap check
     boolean doubleBackToExitPressedOnce = false;
